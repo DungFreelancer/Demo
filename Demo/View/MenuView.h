b33c8a58859d1615bfab1bc3f04093b4b6c3bf84
DungFreelancer <dungdo11051991@gmail.com>
2016-10-29 18:15:27 +0700

    + Add: MenuView.

---
+++ Demo/View/MenuView.h
@@ -0,0 +1,13 @@
+//
+//  MenuView.h
+//  Demo
+//
+//  Created by Dung Do on 10/29/16.
+//  Copyright © 2016 Dung Do. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@interface MenuView : BaseView
+
+@end
---
+++ Demo/View/MenuView.m
@@ -0,0 +1,25 @@
+//
+//  MenuView.m
+//  Demo
+//
+//  Created by Dung Do on 10/29/16.
+//  Copyright © 2016 Dung Do. All rights reserved.
+//
+
+#import "MenuView.h"
+
+@implementation MenuView
+
+- (void)viewDidLoad {
+    self.navigationController.navigationBarHidden = NO;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+}
+
+@end
