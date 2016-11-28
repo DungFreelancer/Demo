96649a5831298341b6e4a4cd23f6e39244e8feb0
DungFreelancer <dungdo11051991@gmail.com>
2016-11-28 23:19:52 +0700

    + Add: ProductsManagerView.

---
+++ Demo/View/ProductsManagerView.m
@@ -0,0 +1,19 @@
+//
+//  ProductsManager.m
+//  Demo
+//
+//  Created by Dung Do on 11/28/16.
+//  Copyright © 2016 Dung Do. All rights reserved.
+//
+
+#import "ProductsManager.h"
+
+@implementation ProductsManagerView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setBackBarItem];
+}
+
+@end
