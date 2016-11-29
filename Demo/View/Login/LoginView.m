2afb923355e0a84537a3f75c56e2515f47f2a029
DungFreelancer <dungdo11051991@gmail.com>
2016-11-29 21:20:04 +0700

    - Change: test account.

--- Demo/View/Login/LoginView.m
+++ Demo/View/Login/LoginView.m
@@ -38,7 +38,7 @@
     [self checkLoginStatus];
     
     // Template input
-    [self.txtUserName setText:@"hai"]; //cap1, cap2
+    [self.txtUserName setText:@"cap2"];
     [self.txtPassword setText:@"123456"];
 //    [self onClickLogin:nil];
 }
