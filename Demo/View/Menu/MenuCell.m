96acdd790577d813c1b86c400e9bb73cbf5e8d05
DungFreelancer <dungdo11051991@gmail.com>
2016-12-11 13:50:31 +0700

    + Add: Badge icon for event menu.

---
+++ Demo/View/Menu/MenuCell.m
@@ -0,0 +1,20 @@
+//
+//  MenuCell.m
+//  Demo
+//
+//  Created by Dung Do on 12/11/16.
+//  Copyright © 2016 Dung Do. All rights reserved.
+//
+
+#import "MenuCell.h"
+#import "UIView+RoundedCorners.h"
+
+@implementation MenuCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self.viewBadgeBG applyRoundedCornersFull];
+}
+
+@end
