beb5b07a0606be731b4a730b4400e411ddfb4775
DungFreelancer <dungdo11051991@gmail.com>
2016-12-16 23:25:19 +0700

    + Add: ContactsView.

---
+++ HAI/View/Contacts.h
@@ -0,0 +1,13 @@
+//
+//  Contacts.h
+//  HAI
+//
+//  Created by Dung Do on 12/16/16.
+//  Copyright © 2016 Dung Do. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@interface ContactsView : BaseView
+
+@end
---
+++ HAI/View/Contacts.m
@@ -0,0 +1,19 @@
+//
+//  Contacts.m
+//  HAI
+//
+//  Created by Dung Do on 12/16/16.
+//  Copyright © 2016 Dung Do. All rights reserved.
+//
+
+#import "Contacts.h"
+
+@implementation ContactsView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [super setBackBarItem];
+}
+
+@end
